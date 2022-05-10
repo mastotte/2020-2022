56d6f9f303a1e72c4f7d450194643a918b0187d7
Maxwell Totten <mastotte@ucsc.edu>
2022-05-10 03:40:25 +0000

    Update Double.h

--- Sol/Double.h
+++ Sol/Double.h
@@ -2,8 +2,8 @@
 //
 //	Double.h
 
-void doub(int players, int rounds, int TESTSIZE, bool isTest);
+void doub(int players, int rounds, int TESTSIZE, bool isTest, FILE* p);
 
 float calculateSD_2(float data[], float p);
 
-void output_2(int best_round, int n, int best[n][n], int rounds, bool isTest);
+void output_2(int best_round, int n, int best[n][n], int rounds, bool isTest, FILE* p);
