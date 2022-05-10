c58cef54c28ae9679e393e3d8fe2666b0f2214da
Max Totten <mastotte@unix5.lt.ucsc.edu>
2022-05-09 20:39:45 -0700

    .h

--- Sol/Double.h
+++ Sol/Double.h
@@ -2,8 +2,8 @@
 //
 //	Double.h
 
-void doub(int players, int rounds);
+void doub(int players, int rounds, int TESTSIZE, bool isTest);
 
 float calculateSD_2(float data[], float p);
 
-void output_2(int best_round, int n, int best[n][n], int rounds);
+void output_2(int best_round, int n, int best[n][n], int rounds, bool isTest);
--- Sol/Quad.h
+++ Sol/Quad.h
@@ -2,8 +2,8 @@
 //
 //	Game.h
 
-void quad(int players, int rounds);
+void quad(int players, int rounds, int TESTSIZE, bool isTest);
 
 float calculateSD_4(float data[], float p);
 
-void output_4(int best_round, int n, int best[n][n], int rounds);
+void output_4(int best_round, int n, int best[n][n], int rounds, bool isTest);
