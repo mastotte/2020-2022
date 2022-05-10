7889e1a45b13998538281fb4f6ad85883df87af9
Maxwell Totten <mastotte@ucsc.edu>
2022-05-10 03:40:56 +0000

    Update Quad.h

--- Sol/Quad.h
+++ Sol/Quad.h
@@ -1,9 +1,9 @@
 //
 //
-//	Game.h
+//	Quad.h
 
-void quad(int players, int rounds, int TESTSIZE, bool isTest);
+void quad(int players, int rounds, int TESTSIZE, bool isTest, FILE* p);
 
 float calculateSD_4(float data[], float p);
 
-void output_4(int best_round, int n, int best[n][n], int rounds, bool isTest);
+void output_4(int best_round, int n, int best[n][n], int rounds, bool isTest, FILE* p);
