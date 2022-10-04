60baaf92005565b9d544c47564812392ceae9937
Maxwell Totten <mastotte@ucsc.edu>
2022-10-04 13:51:41 -0700

    Update linkedlist.cpp

--- 101(pt2)/hw_1/linkedlist.cpp
+++ 101(pt2)/hw_1/linkedlist.cpp
@@ -8,7 +8,7 @@
 #include "linkedlist.h"
 #include <cstdlib>
 #include <iostream>
-#include <vector>
+#include <vector> 
 #include <algorithm>
 #include <string>
 using namespace std;
@@ -16,11 +16,12 @@ using namespace std;
 // Default constructor sets head and tail to null
 List :: List()
 {
-        head = NULL;
+	head = NULL;
+	length = 0;
 }
 // ACCESS FUNCTIONS------------------------------------------------------
 Node* List :: getHead(){
-        return head;
+	return head;
 }
 // Insert(int val): Inserts the int val into list, at the head of the list. Note that there may be multiple copies of val in the list.
 // Input: Int to insert into the linked list
@@ -33,88 +34,95 @@ void List :: prepend(int rank, int freq, string word)
     to_add->word = word;
     to_add->next = head; // make to_add point to existing head
     head = to_add; // set head to to_add
+    length++;
 }
 void List :: append(int rank, int freq, string word)
 {
     if(head == NULL){
-            cout<<"Warning! Appending NULL head!"<<endl;
+	    cout<<"Warning! Appending NULL head!"<<endl;
     }
     Node* to_add = new Node;
     to_add->rank = rank;
     to_add->freq = freq;
     to_add->word = word;
     to_add->next = NULL;
-
+    
     Node* cursor = head;
     while(cursor->next != NULL){
-            cursor = cursor->next;
+	    cursor = cursor->next;
     }
     cursor->next = to_add;
+    length++;
 }
 void List :: sort()
 {
-    Node* cursor = head;
+    Node* cursor;
     string temp_word;
     int temp_freq;
-    int rank_setter = 0;
-    while(cursor != NULL){
-        cursor->rank = rank_setter;
-        if(cursor->next!=NULL){
-            if(cursor->freq < cursor->next->freq){
-                   // SWAP NODES
-                   temp_freq = cursor->freq;
-                   temp_word = cursor->word;
+    int swap_count = 1;
+    while(swap_count!=0){//while 1
+	swap_count = 0;
+	cursor = head;
+        while(cursor != NULL){//while 2
+	    if(cursor->next!=NULL){// if 1
+	        if(cursor->freq < cursor->next->freq){//if 2
+		   // SWAP NODES
+		   swap_count++;
+		   temp_freq = cursor->freq;
+		   temp_word = cursor->word;
 
-                   cursor->freq = cursor->next->freq;
-                   cursor->word = cursor->next->word;
+		   cursor->freq = cursor->next->freq;
+		   cursor->word = cursor->next->word;
 
-                   cursor->next->freq = temp_freq;
-                   cursor->next->word = temp_word;
-
-            }
-        }
-        rank_setter++;
-        cursor = cursor->next;
-    }
-}
+		   cursor->next->freq = temp_freq;
+		   cursor->next->word = temp_word;
+		     
+	        }else if(cursor->freq == cursor->next->freq){
+		    if(cursor->word > cursor->next->word){
+			temp_word = cursor->word;
+			cursor->word = cursor->next->word;
+			cursor->next->word = temp_word;
+			swap_count++;
+		    }
+		}//if 2
+	    }//if 1
+	    cursor = cursor->next;
+	    
+	}//while 2
+    }//while 1
+    // Sort alphabetically
+}    
 // find(int val): Finds a Node with data "val"
 // Input: int to be found
 // Output: a pointer to a Node containing val, if it exists. Otherwise, it returns NULL
-// Technically, it finds the first Node in the list containing val
 Node* List :: find(string word)
 {
-    Node *curr = head;
+    Node *curr = head; 
     while(curr != NULL)
     {
         if (curr->word == word) // found val, so return curr
             return curr;
         curr = curr->next; // otherwise, proceed through list
     }
-    // if loop terminates, val not found
     return NULL;
 }
 
 // deleteNode(int val): Delete a Node with data val, if it exists. Otherwise, do nothing.
 // Input: int to be removed
-// Output: pointer to Node that was deleted. If no Node is deleted, return NULL. If there are multiple Nodes with val, only the first Node in the list is deleted. Note that this removes the node from the list
-//         but does not free the corresponding memory
+// Output: pointer to Node that was deleted.
 Node* List :: deleteNode(string val)
 {
     Node* prev = NULL;
     Node* curr = head;
     while(curr != NULL) // loop over list, starting from head
     {
-        if(curr->word == val) // we found Node with val, so break
-            break;
-        //otherwise, proceed list
+        if(curr->word == val)
+            break; 
         prev = curr; // move prev by one Node
         curr = curr->next; // move curr by one Node
     }
-    // at this point, curr points to Node with val. If curr is null, then val is not in the list.
     if (curr == NULL) // val not found
-        return NULL;
-    // val is in list. Note that curr is not NULL. There is a case analysis now. If prev is null, then curr is head. So we delete head directly.
-    // Otherwise, we delete the Node after prev.
+        return NULL; 
     if (prev == NULL) // we need to delete head
         head = head->next; // delete head
     else // we delete Node after prev. Note that both curr and prev are not NULL
@@ -122,20 +130,18 @@ Node* List :: deleteNode(string val)
     curr->next = NULL; // set next pointer of curr to NULL, to ensure no dangling references
     return curr;
 }
-
-
 // deleteAndFreeNode(int val): Delete a Node with data val and free the memory, if it exists. Otherwise, do nothing.
 // Input: int to be removed
-// Output: void. If there are multiple Nodes with val, only the first Node in the list is deleted. Note that this frees the memory of that deleted node
+// Output: void. If there are multiple Nodes with val, only the first Node in the list is deleted.
 void List :: deleteAndFreeNode(string val)
 {
     Node* ptr = deleteNode(val); // get pointer to node with val
     if (ptr != NULL) // if node actually exists
     {
         ptr->next = NULL; // just be extra safe, probably don't need this
-        delete(ptr); // free that memory
+        delete(ptr);
     }
-    return; //DONE!
+    return; 
 }
 
 // Deletes every Node to prevent memory leaks.
@@ -143,16 +149,14 @@ void List :: deleteAndFreeNode(string val)
 // Output: Void, just deletes every Node of the list
 void List :: deleteList()
 {
-        Node *curr = head; // curr is the current Node as it progresses through the linked list. Initialized to head to start at the head of the list
+    Node *curr = head;
     Node *temp = NULL;
-
-        // curr will loop over list. At every iteration, we first store a pointer to the next Node, and then delete the current Node.
     while(curr != NULL){ // Looping over list
-        temp = curr->next; // store pointer to next Node in the list
-        delete(curr); // delete the current Node
-        curr = temp; // update curr to proceed through list
-    }
-    head = NULL;
+		temp = curr->next; // store pointer to next Node in the list
+		delete(curr); // delete the current Node
+		curr = temp; // update curr to proceed through list
+	}
+	head = NULL;
 }
 
 // Prints list in order
@@ -161,12 +165,11 @@ void List :: deleteList()
 string List :: print()
 {
     string list_str = ""; // string that has list
-        Node *curr = head; // curr is the current Node as it progresses through the linked list. Initialized to head to start at the head of the list
-        // curr will loop over list. It prints the content of curr, and then moves curr to next Node.
-        while(curr != NULL){ // looping over list
+	Node *curr = head;
+	while(curr != NULL){ // looping over list
         list_str = list_str + curr->word + " "; //append string with current Node's data
-                curr = curr->next; // proceed through list
-        }
+		curr = curr->next; // proceed through list
+	}
     if (list_str.length() > 0) // string is non-empty
         list_str.pop_back(); // remove the last (extra) space from string
     return list_str;
@@ -175,15 +178,8 @@ string List :: print()
 // Computes the length of the linked list
 // Input: None
 // Output: Int, length of list
-int List :: length()
+int List :: getLength()
 {
-    int length = 0; // initialize length to zero
-        Node *curr = head; // curr is ths current Node as it progresses through the linked list. Initialized to head to start at the head of the list
-
-        while(curr != NULL){ // looping over list
-                length++; // increment length
-                curr = curr->next; // proceed to next Node
-        }
-        return length;
+    return length;
 }
- 
+
