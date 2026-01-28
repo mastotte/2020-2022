5cea1ce85513801a58144a86ef79eee43e5960cb
Max Totten <147641361+mastotte@users.noreply.github.com>
2026-01-28 13:09:27 -0800

    Implement depth_of_value function in three_template.c
    Added function to determine the depth of a value in a binary tree.

---
+++ 13/final/three_template.c
@@ -0,0 +1,14 @@
+#include "final.h"
+
+#include <stdio.h>
+
+int depth_of_value(int value, BinaryTree *tree) {
+  // Your code here!
+  // Remember -- return 0 if the value is found at the root node, and -1 if the
+  // value is not found in the tree at all.
+  // You don't have to handle trees where the same value occurs more than once,
+  // but if it does happen, you ought to return the shallowest depth of that
+  // value.
+  // Also remember that this is just a binary tree, not a binary *search* tree.
+  return 0;
+}
