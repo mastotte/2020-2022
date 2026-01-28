8fc009b3e312d353ab5982d1dca6edfb135f4ab0
Max Totten <147641361+mastotte@users.noreply.github.com>
2026-01-28 13:10:18 -0800

    Create one_template.c

---
+++ 13/final/one_template.c
@@ -0,0 +1,10 @@
+#include "final.h"
+
+#include <stdio.h>
+
+size_t word_count(char *text) {
+  // Your code here!
+  // Count up all the words that occur in the given string. A new word starts
+  // when you see a letter a-z or A-Z.
+  return 0;
+}
